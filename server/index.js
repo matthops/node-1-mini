bdfd35a0e7ad22caed8fe081d7e91572a3ce32f2
Matt Hopkins <matthops@gmail.com>
2018-07-30 12:36:48 -0500

    completed server and books controller buildout

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const bc = require("./controllers/books_controller.js");
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/../build"));
+
+app.get("/api/books/", bc.read);
+app.post("/api/books/", bc.create);
+app.put("/api/books/:id", bc.update);
+app.delete("/api.books/:id", bc.delete);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
